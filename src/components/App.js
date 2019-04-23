a5d3395fc3d3aca304094e9840f501b2cd3e462f
yuhei <yuhei39.apollo@gmail.com>
2019-04-23 20:04:15 +0900

    add default func

--- src/components/App.js
+++ src/components/App.js
@@ -10,12 +10,31 @@ export default class App extends Component {
             todo: []
         };
         this.handleAdd = this.handleAdd.bind(this);
+        this.handleRemove = this.handleRemove.bind(this);
     }
 
     handleAdd(e) {
-        console.log(e.target.title.value);
+        // console.log(e.target.title.value);
+
+        // リダイレクト防止
         e.preventDefault();
+        // フォームから受け取ったデータをオブジェクトにオブジェクトに挿入して，stateのtodo配列に追加
+        this.state.todo.push({title: e.target.title.value});
+        // setStateを使ってstateを上書き
+        this.setState({todo: this.state.todo});
+
+        e.target.title.value = '';
     }
+
+    // データ削除
+    handleRemove(i){
+        // todo配列からi番目から1つ目のデータを除外
+        this.state.todo.splice(i,1);
+        // setStateでtodo配列を上書き
+        this.setState({todo: this.state.todo});
+    }
+
+
 
     render() {
         return (
@@ -24,7 +43,7 @@ export default class App extends Component {
                     <h1 className="simple-box-title simple--color-white">React Todo App</h1>
                     <Form handleAdd={this.handleAdd}/>
                     <div className="simple-rule"></div>
-                    <List/>
+                    <List todos={this.state.todo} handleRemove={this.handleRemove}/>
                 </div>
             </div>
         );
--- src/components/List.js
+++ src/components/List.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 let style = {
     maxWidth: '700px',
@@ -7,12 +7,13 @@ let style = {
 let btn = {
     cursor: 'pointer'
 };
-
 const List = (props) => (
     <ul className="siimple-list">
-        <li className="siimple-list-item siimple--bg-white" style={style}>Item 1 <span className="siimple-tag siimple-tag--error siimple-hover" style={btn}>Delete</span></li>
-        <li className="siimple-list-item siimple--bg-white" style={style}>Item 2 <span className="siimple-tag siimple-tag--error siimple-hover" style={btn}>Delete</span></li>
-        <li className="siimple-list-item siimple--bg-white" style={style}>Item 3 <span className="siimple-tag siimple-tag--error siimple-hover" style={btn}>Delete</span></li>
+        {props.todos.map((todo, i) => {
+            return <li key={i} className="" style={style}>{todo.title} <span
+                className="" style={btn}
+                onClick={() => props.handleRemove(i)}>Delete</span></li>
+        })}
     </ul>
 );
 
